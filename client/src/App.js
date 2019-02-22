fae4b88a2c3dd101e4ad35816a5c80e1b064e50b
destin floyd <destin.b.floyd@gmail.com>
2019-02-22 11:40:58 -0500

    react can proxy and get data from django api, now starting to make componets. added a git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+// import X from "./components/X";
+import { BrowserRouter as Router} from 'react-router-dom'
+// , Switch, Route 
 
 class App extends Component {
+
+
+  onCheck = () => {
+    axios.get("api/p4/users/").then(res=>{
+      console.log(res.data)
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+
+      <Router>
+        <div>
+          <div>
+            <button onClick={this.onCheck()}> Check users</button>
+
+          </div >
+          {/* <Switch> */}
+
+            {/* // <Route exact path='/' component={StackList} /> */}
+            {/* // <Route exact path='/all/:setId' component={SingleStack} /> */}
+
+          {/* </Switch> */}
+
+        </div>
+      </Router>
+
+
     );
   }
 }
