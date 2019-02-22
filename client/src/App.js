0026bbea8d067ec3cdbb1a9e5fbf345686e3160d
destin floyd <destin.b.floyd@gmail.com>
2019-02-22 15:25:36 -0500

    react componet 1 is getting and showing API data

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
-// import X from "./components/X";
-import { BrowserRouter as Router} from 'react-router-dom'
-// , Switch, Route 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import User from './components/User';
 
 class App extends Component {
-
-
+state ={
+  responseStuff : [{userId: 0,
+  name: '',
+  style: '',
+  characters: [{}]
+ }]
+  
+}
+componentDidMount(){
+ this.onCheck()
+}
   onCheck = () => {
-    axios.get("api/p4/users/").then(res=>{
+    axios.get("api/p4/users/").then(res => {
+      this.setState({ responseStuff: res.data });
       console.log(res.data)
     })
   }
+// makeIt =(responseData )=>{
+//   let x = this.state;
+//   x = responseData
+//   this.setState({responseStuff: x})
+// }
+
   render() {
+   const userComponent = ()=>{ return <User users={this.state.responseStuff}/> }
     return (
 
 
       <Router>
         <div>
           <div>
-            <button onClick={this.onCheck()}> Check users</button>
-
+            <button onClick={this.onCheck}> Check users</button>
+          
           </div >
-          {/* <Switch> */}
+          <Switch>
 
-            {/* // <Route exact path='/' component={StackList} /> */}
-            {/* // <Route exact path='/all/:setId' component={SingleStack} /> */}
-
-          {/* </Switch> */}
+            <Route exact path='/' render={userComponent} />
+            {/* <Route exact path='/user/<:userId>' component={User} /> */}
+          </Switch>
 
         </div>
       </Router>
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom'
 
 class User extends Component {
+
     render() {
         return (
             <div>
-                
+
+<p>Hey im user </p>
+{this.props.users.map((user, i) => (
+
+<div key={i}>
+    <div>
+        <div>{user.name}</div>
+        {/* <Link to={`/all/${user._id}`}> */}
+        {/* <div>{user.name}</div> */}
+        {/* </Link> */}
+    </div>
+</div>
+
+))}
             </div>
         );
     }
