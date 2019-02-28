600cc8e78775b4197ffe8d202886710422a86552
destin floyd <destin.b.floyd@gmail.com>
2019-02-28 16:17:29 -0500

    heroku

--- dnd_project/urls.py
+++ dnd_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/p4/', include('dnd_app.urls'))
+    path('api/p4/', include('dnd_app.urls')),
+    url(r'^$', views.FrontendAppView.as_view())
 ]
