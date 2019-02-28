99d1811065a85bb6ce0b0a0861ce039aa00f1acb
destin floyd <destin.b.floyd@gmail.com>
2019-02-28 16:10:04 -0500

    heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dnd_project.wsgi --log-file -
--- dnd_project/settings.py
+++ dnd_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'dnd_app',
+    'django_extensions'
+
 
 ]
 
@@ -76,10 +79,19 @@ WSGI_APPLICATION = 'dnd_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sample_database',
+        'USER': 'destin',
+        'PASSWORD': 'CodeBridge',
+        'HOST': 'localhost'
     }
 }
 
@@ -129,4 +141,5 @@ STATIC_URL = '/static/'
 #     ]
 # }
 # took out so unauthenticated users can post. 
+django_heroku.settings(locals())
 
